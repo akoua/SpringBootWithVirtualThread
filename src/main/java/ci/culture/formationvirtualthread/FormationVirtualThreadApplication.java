ad41ad21b0189a4cbaf50cd05ff0a667bc72b4db
akoua <akouayao225@gmail.com>
2024-05-19 20:22:44 +0200

    first commit

---
+++ src/main/java/ci/culture/formationvirtualthread/FormationVirtualThreadApplication.java
@@ -0,0 +1,13 @@
+package ci.culture.formationvirtualthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationVirtualThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationVirtualThreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ci/culture/formationvirtualthread/FormationVirtualThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.culture.formationvirtualthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationVirtualThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
